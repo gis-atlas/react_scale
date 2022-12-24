7a990997a5a18eb576528a0fc5d15bea373a1344
zzZJagerZzz <tokyo-jager@yandex.ru>
2022-12-24 16:34:10 +0300

    add mixins

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,26 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <a href='/asd'>sad</a>
+      <ul>
+        <li>sd</li>
+        <li>fg</li>
+        <li>sdfas</li>
+        <li>sdgf</li>
+      </ul>
+      <ol>
+        <li>sd</li>
+        <li>fg</li>
+        <li>sdfas</li>
+        <li>sdgf</li>
+      </ol>
+      <input placeholder='sdads' />
+      <button>adasd</button>
+      <h1>Подписка</h1>
+      <h2>О сервисе</h2>
+      <div style={{ background: 'red' }} className='square'></div>
+    </div>
+  );
 };
 
 export default Home;
