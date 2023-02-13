14b9601950ce681a341c7830639ab05fddf8699b
Ruslan Ivlev <91348490+zzZJagerZzz@users.noreply.github.com>
2023-02-13 18:43:52 +0300

    delete unused variables

--- src/components/UI/Input/Input.tsx
+++ src/components/UI/Input/Input.tsx
@@ -1,4 +1,4 @@
-import { Ref, forwardRef, useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import classNames from 'classnames';
 import './index.sass';
 
@@ -24,95 +24,93 @@ interface IInput {
   onAppendIconClick?: any;
 }
 
-const Input = forwardRef(
-  ({
-    name = '',
-    label = '',
-    placeholder = '',
-    defaultValue = '',
-    onInput = () => {},
-    styles,
-    className,
-    type = 'text',
-    useGradient = true,
-    readonly = false,
-    prevIcon,
-    onPrevIconClick,
-    appendIcon,
-    onAppendIconClick,
-  }: IInput) => {
-    const [focused, setFocused] = useState<boolean>(false);
-    const [filled, setFilled] = useState<boolean>(false);
-    const [inputValue, setInputValue] = useState<string>(defaultValue);
-    const inputRef = useRef<HTMLInputElement>(null);
-    const onFocus = () => {
-      setFocused(true);
-    };
-    const onBlur = () => {
-      setFocused(false);
-    };
-    const onChange = (e: any) => {
-      const target = e.target as HTMLInputElement;
-      setInputValue(target.value);
-      if (target.value.trim() !== '') {
-        setFilled(true);
-      } else {
-        setFilled(false);
-      }
-      onInput(target.value);
-    };
+const Input = ({
+  name = '',
+  label = '',
+  placeholder = '',
+  defaultValue = '',
+  onInput = () => {},
+  styles,
+  className,
+  type = 'text',
+  useGradient = true,
+  readonly = false,
+  prevIcon,
+  onPrevIconClick,
+  appendIcon,
+  onAppendIconClick,
+}: IInput) => {
+  const [focused, setFocused] = useState<boolean>(false);
+  const [filled, setFilled] = useState<boolean>(false);
+  const [inputValue, setInputValue] = useState<string>(defaultValue);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const onFocus = () => {
+    setFocused(true);
+  };
+  const onBlur = () => {
+    setFocused(false);
+  };
+  const onChange = (e: any) => {
+    const target = e.target as HTMLInputElement;
+    setInputValue(target.value);
+    if (target.value.trim() !== '') {
+      setFilled(true);
+    } else {
+      setFilled(false);
+    }
+    onInput(target.value);
+  };
 
-    useEffect(() => {
-      inputValue.length !== 0 && setFilled(true);
-    }, []);
-    return (
-      <div
-        className={classNames('custom-input', {
-          [`${className}`]: className,
+  useEffect(() => {
+    inputValue.length !== 0 && setFilled(true);
+  }, []);
+  return (
+    <div
+      className={classNames('custom-input', {
+        [`${className}`]: className,
+      })}
+      style={styles}
+    >
+      <label
+        className={classNames({
+          focused: focused || filled,
+          'icon-prev': prevIcon,
+          'icon-append': appendIcon,
         })}
-        style={styles}
       >
-        <label
-          className={classNames({
-            focused: focused || filled,
-            'icon-prev': prevIcon,
-            'icon-append': appendIcon,
-          })}
-        >
-          {prevIcon && <img src={prevIcon} alt='' className='icon icon-prev' />}
-          <input
-            ref={inputRef}
-            name={name}
-            onFocus={onFocus}
-            onBlur={onBlur}
-            onChange={onChange}
-            placeholder={(focused || !label) && !readonly ? placeholder : ''}
-            type={type}
-            value={inputValue}
-            readOnly={readonly}
-            autoComplete='off'
+        {prevIcon && <img src={prevIcon} alt='' className='icon icon-prev' />}
+        <input
+          ref={inputRef}
+          name={name}
+          onFocus={onFocus}
+          onBlur={onBlur}
+          onChange={onChange}
+          placeholder={(focused || !label) && !readonly ? placeholder : ''}
+          type={type}
+          value={inputValue}
+          readOnly={readonly}
+          autoComplete='off'
+        />
+        {!readonly && (
+          <span
+            className={classNames({
+              gradient: useGradient,
+            })}
+          >
+            {label}
+          </span>
+        )}
+        {appendIcon && (
+          <img
+            src={appendIcon}
+            alt=''
+            className='icon icon-append'
+            onClick={() => onAppendIconClick(inputRef.current)}
           />
-          {!readonly && (
-            <span
-              className={classNames({
-                gradient: useGradient,
-              })}
-            >
-              {label}
-            </span>
-          )}
-          {appendIcon && (
-            <img
-              src={appendIcon}
-              alt=''
-              className='icon icon-append'
-              onClick={() => onAppendIconClick(inputRef.current)}
-            />
-          )}
-        </label>
-      </div>
-    );
-  }
-);
+        )}
+      </label>
+    </div>
+  );
+};
 
 export default Input;
